aaba642f5c07b0ff646e601008d998dfc375c835
orbb4 <reiturra2021@udec.cl>
2025-04-26 00:16:21 -0400

    Se organizan elementos para editar clima con grid

--- src/pages/weather/edit.js
+++ src/pages/weather/edit.js
@@ -1,15 +1,43 @@
+import { useState } from "react";
+import "@/styles/editWeather.css";
 export default function EditWeather() {
+  const [temperature, setTemperature] = useState("30");
     return (
       
-      <div>
-        <h1>Ingrese el clima de su localidad</h1>
-        <p>Estado del tiempo</p>
-        <select style={{color: "black", backgroundColor: "white"}}>
-          <option value="Soleado">Soleado</option>
-          <option value="Parcialmente Nublado">Parcialmente Nublado</option>
-          <option value="Nublado">Nublado</option>
-          <option value="Lluvioso">Lluvioso</option>
-        </select>
-      </div>
+      <div className="weather-container">
+          <div className="weather-page">
+            <div className="title">
+              <header>
+                <h1>Ingrese el clima de su localidad</h1>
+              </header>
+            </div>
+
+            <div className="left-label">
+              <p>Estado del tiempo</p>
+            </div>
+            <div className="left">
+              <select style={{ color: "black", backgroundColor: "white" }}>
+                <option value="Soleado">Soleado</option>
+                <option value="Parcialmente Nublado">Parcialmente Nublado</option>
+                <option value="Nublado">Nublado</option>
+                <option value="Lluvioso">Lluvioso</option>
+              </select>
+            </div>
+
+            <div className="right-label">
+              <p>Temperatura</p>
+            </div>
+            <div className="right">
+              <input
+                type="number"
+                style={{ height: 40, padding: 5, color: "black", backgroundColor: "white" }}
+                placeholder="22°"
+                onChange={(e) => setTemperature(e.target.value)}
+                value={temperature}
+              />
+            </div>
+          </div>
+          <button className="next-button">Siguiente</button>
+        </div>
     );
   }
